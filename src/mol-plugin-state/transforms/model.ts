24b017bc096b18b58211cc13e12facb5dde409d8
Russ Taylor <russ@reliasolve.com>
2026-02-17 09:28:05 -0500

    Renaming the Kinemage shape provider pipeline to include the name lines so we can make separate ones for meshes and balls

--- src/mol-plugin-state/transforms/model.ts
+++ src/mol-plugin-state/transforms/model.ts
@@ -12,7 +12,7 @@ import { parseGRO } from '../../mol-io/reader/gro/parser';
 import { parsePDB } from '../../mol-io/reader/pdb/parser';
 import { Mat4, Vec3 } from '../../mol-math/linear-algebra';
 import { shapeFromPly } from '../../mol-model-formats/shape/ply';
-import { shapeFromKin } from '../../mol-model-formats/shape/kin';
+import { shapeLinesFromKin } from '../../mol-model-formats/shape/kin';
 import { coordinatesFromDcd } from '../../mol-model-formats/structure/dcd';
 import { trajectoryFromGRO } from '../../mol-model-formats/structure/gro';
 import { trajectoryFromCCD, trajectoryFromMmCIF } from '../../mol-model-formats/structure/mmcif';
@@ -92,7 +92,7 @@ export { StructureComponent };
 export { CustomModelProperties };
 export { CustomStructureProperties };
 export { ShapeFromPly };
-export { ShapeFromKin };
+export { ShapeLinesFromKin };
 
 type CoordinatesFromDcd = typeof CoordinatesFromDcd
 const CoordinatesFromDcd = PluginStateTransform.BuiltIn({
@@ -1214,10 +1214,10 @@ const ShapeFromPly = PluginStateTransform.BuiltIn({
     }
 });
 
-type ShapeFromKin = typeof ShapeFromKin
-const ShapeFromKin = PluginStateTransform.BuiltIn({
-  name: 'shape-from-kin',
-  display: { name: 'Shape from KIN', description: 'Create Shape from KIN data' },
+type ShapeLinesFromKin = typeof ShapeLinesFromKin
+const ShapeLinesFromKin = PluginStateTransform.BuiltIn({
+  name: 'shape-lines-from-kin',
+  display: { name: 'Shape Lines from KIN', description: 'Create Shape from KIN data' },
   from: SO.Format.Kin,
   to: SO.Shape.Provider,
   params(a) {
@@ -1229,7 +1229,7 @@ const ShapeFromKin = PluginStateTransform.BuiltIn({
 })({
   apply({ a, params }) {
     return Task.create('Create shape from KIN', async ctx => {
-      const shape = await shapeFromKin(a.data, params).runInContext(ctx);
+      const shape = await shapeLinesFromKin(a.data, params).runInContext(ctx);
       const props = { label: params.label || 'Shape' };
       return new SO.Shape.Provider(shape, props);
     });
